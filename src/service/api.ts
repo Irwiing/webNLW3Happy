3ba1c2c2bb5a72a7aa28fb8f40de68848648c41f
Irwiing <irwingtguarnieri@gmail.com>
2020-10-18 21:43:23 -0300

    .

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://irwing-happy-nlw3.herokuapp.com/',
+    baseURL: 'https://irwing-happy-nlw3.herokuapp.com',
 })
 
 export default api;
